7da14c3ad8998561561ad90f78595fe4449af98f
Danny Perez <daniel.perez@structuredweb.com>
2017-09-19 18:05:34 -0400

    added the ajax call and button to get attr

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -1,14 +1,14 @@
 var gifs = ["Dogs", "Cats", "Surprise", "Funny"]; //this array will be the buttons. 
+var giphyBaseUrl = "https://api.giphy.com/v1/gifs/search";
+var APIkey = "nDLLdhwCN42tVLQMGumeixniRSkgpv1l";
 
 //helper functions ------------------------------------------------------------
 
 function renderButtons() { //this will make the buttons appear on the HTML
-
 	$("#buttons-view").empty();
-
 	for (var i = 0; i < gifs.length; i++) {
 		var a = $("<button>");		
-		a.addClass("gif");		
+		a.addClass("gif-button");		
 		a.attr("data-name", gifs[i]);		
 		a.text(gifs[i]);
 		$("#buttons-view").append(a);
@@ -30,10 +30,21 @@ function aJax(URL, METHOD, CALLBACK){ //creating a ajax method so i can only cal
 	}).done(CALLBACK);
 }
 
+function getGifs(data){
+	var gifArray = data.data;
+	console.log(gifArray);
+}
 
+function getDataType(){
+	console.log(this);
+}
 
 // code starts here ------------------------------------------------
+
+aJax(giphyBaseUrl+"?q=cat&limit=10&api_key="+APIkey, "GET", getGifs);
 
 $("#add-gif").on("click", addGifButton);
 
 renderButtons();
+
+$(".gif-button").on("click", getDataType);
