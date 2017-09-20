e46795d2b3b387ee7ca446fb9a23e4895f9d34f9
danny perez <perez6736@gmail.com>
2017-09-20 00:44:35 -0400

    images load up when clicking the buttons

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -1,17 +1,18 @@
-var gifs = ["Dogs", "Cats", "Surprise", "Funny"]; //this array will be the buttons. 
+var gifs = ["Dog", "Cat", "Surprise", "Funny"]; //this array will be the buttons. 
 var giphyBaseUrl = "https://api.giphy.com/v1/gifs/search";
 var APIkey = "nDLLdhwCN42tVLQMGumeixniRSkgpv1l";
+
 
 //helper functions ------------------------------------------------------------
 
 function renderButtons() { //this will make the buttons appear on the HTML
 	$("#buttons-view").empty();
-	for (var i = 0; i < gifs.length; i++) {
-		var a = $("<button>");		
-		a.addClass("gif-button");		
-		a.attr("data-name", gifs[i]);		
-		a.text(gifs[i]);
-		$("#buttons-view").append(a);
+	for (var i = 0; i < gifs.length; i++) { //loop through array 
+		var a = $("<button>");		// create a button element 
+		a.addClass("gif-button");		//give the element a class
+		a.attr("data-name", gifs[i]);		//give the attr data-name = to the value of the index in the array
+		a.text(gifs[i]); //put the text in the button 
+		$("#buttons-view").append(a); // append the button to the element with id buttonsview 
 	}
 }
 
@@ -30,21 +31,48 @@ function aJax(URL, METHOD, CALLBACK){ //creating a ajax method so i can only cal
 	}).done(CALLBACK);
 }
 
-function getGifs(data){
-	var gifArray = data.data;
-	console.log(gifArray);
+function displayGifs(gifImg, index){
+	var imgContainer = $("<div>");
+	var imgTag = $("<img>"); //create an img tag 
+	imgTag.attr("src", gifImg) //gif the src the img url 
+	imgContainer.addClass("image-container"); //give the container a class
+	imgContainer.attr("data-indexed", index); // give the contianer tag an attr of the index it was in the array 
+	$("#gif-view").append(imgContainer); //append the container to the gif view
+	imgContainer.append(imgTag);
+	
+}
+
+function getGifs(response){
+	var gifArray = response.data; //data is the array in the object returned from giphy 
+	for(i=0; i<gifArray.length; i++){ //loop through each index of data or each gif
+		// get the still image of the gif and display it giving an attribute of the index it was in the array. 
+		displayGifs(gifArray[i].images.original_still.url, i);
+	}
 }
 
 function getDataType(){
-	console.log(this);
+	
+	var gifType = ($(this).attr("data-name")); //get data name from button
+	// make the url with the data type 
+	var queryURL = giphyBaseUrl+"?q="+gifType+"&limit=10&api_key="+APIkey;
+	//ajax call. 
+	aJax(queryURL, "GET", getGifs);
+}
+
+function playGif(){
+	console.log("hello");
 }
 
 // code starts here ------------------------------------------------
 
-aJax(giphyBaseUrl+"?q=cat&limit=10&api_key="+APIkey, "GET", getGifs);
 
-$("#add-gif").on("click", addGifButton);
 
 renderButtons();
 
+$("#add-gif").on("click", addGifButton);
+
 $(".gif-button").on("click", getDataType);
+ 
+
+ // this doesnt work----why?
+$(".image-container").on("click", playGif);
