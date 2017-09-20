bbddececda265a714f4ce0e021d29611706b3430
Danny Perez <daniel.perez@structuredweb.com>
2017-09-20 11:59:32 -0400

    added on click handler to play and stop gifs

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -1,12 +1,13 @@
 var gifs = ["Dog", "Cat", "Surprise", "Funny"]; //this array will be the buttons. 
 var giphyBaseUrl = "https://api.giphy.com/v1/gifs/search";
 var APIkey = "nDLLdhwCN42tVLQMGumeixniRSkgpv1l";
+var gifArray;
 
 
 //helper functions ------------------------------------------------------------
 
 function renderButtons() { //this will make the buttons appear on the HTML
-	$("#buttons-view").empty();
+	$(".gif-button").detach();
 	for (var i = 0; i < gifs.length; i++) { //loop through array 
 		var a = $("<button>");		// create a button element 
 		a.addClass("gif-button");		//give the element a class
@@ -32,18 +33,21 @@ function aJax(URL, METHOD, CALLBACK){ //creating a ajax method so i can only cal
 }
 
 function displayGifs(gifImg, index){
-	var imgContainer = $("<div>");
+	var imgContainer = $("<div>"); //create a div
 	var imgTag = $("<img>"); //create an img tag 
 	imgTag.attr("src", gifImg) //gif the src the img url 
 	imgContainer.addClass("image-container"); //give the container a class
 	imgContainer.attr("data-indexed", index); // give the contianer tag an attr of the index it was in the array 
+	imgTag.attr("data-isplaying", "still");
 	$("#gif-view").append(imgContainer); //append the container to the gif view
 	imgContainer.append(imgTag);
 	
 }
 
-function getGifs(response){
-	var gifArray = response.data; //data is the array in the object returned from giphy 
+function getStillGifs(response){
+	gifArray = response.data; //data is the array in the object returned from giphy 
+	console.log(response);
+	$(".image-container").detach(); //clear the gifview div before adding stuff to it
 	for(i=0; i<gifArray.length; i++){ //loop through each index of data or each gif
 		// get the still image of the gif and display it giving an attribute of the index it was in the array. 
 		displayGifs(gifArray[i].images.original_still.url, i);
@@ -51,16 +55,33 @@ function getGifs(response){
 }
 
 function getDataType(){
-	
 	var gifType = ($(this).attr("data-name")); //get data name from button
 	// make the url with the data type 
 	var queryURL = giphyBaseUrl+"?q="+gifType+"&limit=10&api_key="+APIkey;
 	//ajax call. 
-	aJax(queryURL, "GET", getGifs);
+	aJax(queryURL, "GET", getStillGifs);
+}
+
+function getGifs(response){
+	var gifArray = response.data;
+	console.log(this);
+	console.log(gifArray);
 }
 
 function playGif(){
-	console.log("hello");
+	var childImgTag = $(this).children(); // this is the actual img tag in the div
+	var imgIndex = $(this).attr("data-indexed"); //this is where in the response object we know the gif is in 
+
+	if(childImgTag.attr("data-isplaying") == "still"){ // if its not playing 
+		childImgTag.attr("src", gifArray[imgIndex].images.original.url); //set the src to the moving gif
+		childImgTag.attr("data-isplaying", "playing");	//set the attribute to playing 
+	}
+	else{
+		childImgTag.attr("src", gifArray[imgIndex].images.original_still.url); //else its prob playing so we set the src to the still img
+		childImgTag.attr("data-isplaying", "still"); //set the attribute to still. 
+		console.log("still");
+		
+	}
 }
 
 // code starts here ------------------------------------------------
@@ -69,10 +90,7 @@ function playGif(){
 
 renderButtons();
 
-$("#add-gif").on("click", addGifButton);
-
-$(".gif-button").on("click", getDataType);
+$(document).on("click", "#add-gif", addGifButton); //click handler for adding buttons 
+$(document).on("click", ".gif-button", getDataType); // click handler for getting the images 
+$(document).on("click", ".image-container", playGif); // click handler for playing the gifs 
  
-
- // this doesnt work----why?
-$(".image-container").on("click", playGif);
